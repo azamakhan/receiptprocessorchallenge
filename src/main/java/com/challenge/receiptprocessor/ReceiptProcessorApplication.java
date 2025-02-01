4d870281cad53e3619fbbf048987df1e8bf0afe7
AzamK <azamakhan30@gmail.com>
2025-01-31 19:09:56 -0500

    renamed files

--- src/main/java/com/challenge/receiptprocessor/ReceiptProcessorApplication.java
+++ src/main/java/com/challenge/receiptprocessor/ReceiptProcessorApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ReceiptprocessorApplication {
+public class ReceiptProcessorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ReceiptprocessorApplication.class, args);
+		SpringApplication.run(ReceiptProcessorApplication.class, args);
 	}
 
 }
